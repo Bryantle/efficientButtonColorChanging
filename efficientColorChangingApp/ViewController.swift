6bf7df0589b4e26c756504bf33e6ef410e1c8926
Bryantle <bryantl@berkeley.edu>
2018-01-14 13:44:16 -0800

    More efficient lines for buttons that change colors upon being pressed, a label that increments with buttons selected and a reset button.

--- efficientColorChangingApp/ViewController.swift
+++ efficientColorChangingApp/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var colorList: Array = [ #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.9223954645, blue: 0.05355838877, alpha: 1), #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)]
+    var countButton = 0{
+        didSet{
+            numberLabel.text = "\(countButton)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet var buttonsList: [UIButton]!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBAction func resetButton(_ sender: UIButton) {
+        countButton = 0
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        for i in buttonsList{
+            i.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
     }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        for i in buttonsList{
+            if i == sender{
+                let indexNumber = buttonsList.index(of:sender)!
+                if sender.backgroundColor == colorList[indexNumber]{
+                    sender.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                    countButton -= 1}
+            else{
+        
+                    sender.backgroundColor = colorList[indexNumber]
+                    countButton += 1}
+                }
+                
+            }
+        }
 
-
-}
+    }
+    
 
